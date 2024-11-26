fc223c088de8ea54c7c282f36eaa0d076c2e4156
Valery Carranza <valery.carranza@ucr.ac.cr>
2024-11-26 15:13:17 -0600

    Estos son los archivos del proyecto

---
+++ main.c
@@ -0,0 +1,15 @@
+// main.c
+#include "database.h"
+#include "ui.h"
+#include <stdio.h>
+
+int main() {
+    if (open_database() != 0) {
+        return 1;  // Si no se puede abrir la base de datos, sale del programa
+    }
+
+    setup_ui();
+
+    close_database();
+    return 0;
+}
---
+++ ui.h
@@ -0,0 +1,8 @@
+// ui.h
+#ifndef UI_H
+#define UI_H
+
+// Función para configurar e iniciar la interfaz gráfica
+void setup_ui();
+
+#endif
